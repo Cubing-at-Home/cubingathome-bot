1f57b37d9c2ba57992a41d733867d1020d62bf5e
louismeunier <louismeunier123@gmail.com>
2021-02-19 22:59:31 -0500

    Firebase rather than admin

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const firebase = require("firebase-admin");
 require("dotenv").config();
 
 firebase.initializeApp({
-    credential: admin.credential.cert({
+    credential: firebase.credential.cert({
       "projectId": process.env.FIREBASE_PROJECT_ID,
       "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
