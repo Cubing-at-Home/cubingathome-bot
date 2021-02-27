dfd1e582c6ffc12e1871eee7c5eb44c9917f20f7
louismeunier <louismeunier123@gmail.com>
2021-02-27 11:50:27 -0500

    Update round announcement

--- db/cah/eventListener.js
+++ db/cah/eventListener.js
@@ -6,22 +6,22 @@ async function addRoundListener(client) {
     const CHANNEL_ID = process.env.NODE_ENV === "production" ? "691137105656807544" : "703359739584577579";
     const CHANNEL = client.channels.cache.get(CHANNEL_ID);
 
-    const initialData = await cahDB.collection("competitions").doc("cah2.1").get();
+    const initialData = await cahDB.collection("competitions").doc("cah2").get();
     let previousRounds = initialData.data().rounds;
     console.log(previousRounds)
 
-    cahDB.collection("competitions").doc("cah2.1")
+    cahDB.collection("competitions").doc("cah2")
         .onSnapshot(doc => {
             let rounds = doc.data().rounds;
             rounds.forEach((round, index) => {
                 if (previousRounds[index].isOpen !== round.isOpen) {
                     round.isOpen ? console.log(round.id + " is open!"): console.log(round.id + " is not open!")
                     if (round.isOpen) {
-                        client.user.setActivity(`${round.id}`,{type:"COMPETING"})
+                        client.user.setActivity(`${round.id.replace("-"," ")}`,{type:"COMPETING"})
+                        CHANNEL.send(`@everyone, **${round.id}** is happening now!`)
                     } else {
-                        client.user.setActivity("'The Speed Cubers' on Netflix",{type: "WATCHING"})
+                        client.user.setActivity("Cubing At Home 2.1",{type: "COMPETING"})
                     }
-                    round.isOpen ? CHANNEL.send(`**${round.id}** is now open for competitors!`) : CHANNEL.send(`**${round.id}** is now closed!`);
                 }
             })
             previousRounds = rounds;
