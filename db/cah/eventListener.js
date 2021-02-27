fdac1adabbb78b7e731799cb52ee7745a7b2e20f
louismeunier <louismeunier123@gmail.com>
2021-02-27 11:54:15 -0500

    Update round announcement

--- db/cah/eventListener.js
+++ db/cah/eventListener.js
@@ -6,11 +6,11 @@ async function addRoundListener(client) {
     const CHANNEL_ID = process.env.NODE_ENV === "production" ? "691137105656807544" : "703359739584577579";
     const CHANNEL = client.channels.cache.get(CHANNEL_ID);
 
-    const initialData = await cahDB.collection("competitions").doc("cah2").get();
+    const initialData = await cahDB.collection("competitions").doc("cah2.1").get();
     let previousRounds = initialData.data().rounds;
     console.log(previousRounds)
 
-    cahDB.collection("competitions").doc("cah2")
+    cahDB.collection("competitions").doc("cah2.1")
         .onSnapshot(doc => {
             let rounds = doc.data().rounds;
             rounds.forEach((round, index) => {
